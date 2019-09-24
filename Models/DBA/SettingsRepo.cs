f68419d3431aaff1f1eaf13d114283bf6280496c
haid0109 <51113567+haid0109@users.noreply.github.com>
2019-09-24 14:57:42 +0200

    created SettingsRepo.cs

--- Models/DBA/SettingsRepo.cs
+++ Models/DBA/SettingsRepo.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using _2019_9_3_Dating_app_XAML_.Assets;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _2019_9_3_Dating_app_XAML_.Models.DBA
 {
-    class SettingsRepo
+    class SettingsRepo : Base
     {
+        public void createProfile(string email, string newEmail, string password)
+        {
+            SQLiteConnection Con = new SQLiteConnection(sqlCon);
+            Con.Open();
+            SQLiteCommand SqlCmd = new SQLiteCommand("UPDATE Users " +
+                                                     "SET email = '" + newEmail + "'," +
+                                                     "password = '" + password + "'" +
+                                                     "WHERE email = '" + email + "'", Con);
 
+            SqlCmd.ExecuteNonQuery();
+            Con.Close();
+        }
     }
 }
--- Models/DBA/ViewMatchesRepo.cs
+++ Models/DBA/ViewMatchesRepo.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using _2019_9_3_Dating_app_XAML_.Assets;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _2019_9_3_Dating_app_XAML_.Models.DBA
 {
-    class ViewMatchesRepo
+    class ViewMatchesRepo : Base
     {
     }
 }
